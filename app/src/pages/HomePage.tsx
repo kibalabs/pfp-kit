f1ce646b982b174d7414738075de379062db05fa
Barakat Ajadi <37664545+Ajadi-Abiola@users.noreply.github.com>
2022-03-15 10:07:54 +0100

    KIBA-1392: Implement flow and connect wallet  (#2)
    * connect wallet
    
    * pfp-kit
    
    Co-authored-by: Barakat Ajadi <barakat.a.ajadi@gmail.com>

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,10 +1,83 @@
 import React from 'react';
 
-import { Text } from '@kibalabs/ui-react';
+import { Alignment, Box, Button, ContainingView, Direction, Image, KibaIcon, LayerContainer, PaddingSize, Spacing, Stack, Text } from '@kibalabs/ui-react';
 
+import { useAccount, useOnLinkAccountsClicked } from '../AccountContext';
+import { ImageView } from '../components/ImageView';
+import imageData from '../imageData.json';
 
 export const HomePage = (): React.ReactElement => {
+  const account = useAccount();
+  const [stage, setStage] = React.useState<number>(1);
+  const [imageLink, setImageLink] = React.useState<string>('');
+
+  const onLinkAccountsClicked = useOnLinkAccountsClicked();
+
+  const onConnectWalletClicked = async (): Promise<void> => {
+    await onLinkAccountsClicked();
+  };
+
+  const showImage = (imageUrl) => {
+    setStage(2);
+    if (imageUrl?.startsWith('ipfs://')) {
+      const FormattedImageUrl = imageUrl.replace('ipfs://', 'https://ipfs.io/ipfs/');
+      setImageLink(FormattedImageUrl);
+    }
+  };
+
   return (
-    <Text>PFP Kit</Text>
+    <ContainingView>
+      <Stack direction={Direction.Vertical} isFullHeight={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} padding={PaddingSize.Wide2} shouldAddGutters={true}>
+        <Text variant='header1'>PFP KIT</Text>
+        {stage === 1 && (
+          <Stack>
+            {!account?.address ? (
+              <Button variant='large-secondary' text= 'Connect Your Wallet' onClicked={onConnectWalletClicked} />
+            ) : (
+              <Stack direction={Direction.Vertical} childAlignment={Alignment.Start} contentAlignment={Alignment.Center} shouldAddGutters={true} defaultGutter={PaddingSize.Wide}isScrollableHorizontally={true}>
+                <Text variant='header3'>Choose your picture </Text>
+                <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true} shouldWrapItems={true}>
+                  {imageData && imageData.map((image, index: number) : React.ReactElement => (
+                    <ImageView
+                      onClicked={showImage}
+                      key={index}
+                      name={image.name}
+                      imageUrl={image.imageUrl}
+                    />
+                  ))}
+                </Stack>
+              </Stack>
+            )}
+          </Stack>
+        )}
+        {stage === 2 && (
+          <Stack direction={Direction.Vertical} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true}>
+            <Box height='300px'>
+              <LayerContainer>
+                <LayerContainer.Layer isFullHeight={false} isFullWidth={false} alignmentVertical={Alignment.Center} alignmentHorizontal={Alignment.Center}>
+                  <Box variant='rounded-wideBorder' shouldClipContent={true} width='160px' height='160px'>
+                    <Image source='/assets/icon.png' alternativeText='image' fitType='contain' />
+                  </Box>
+                </LayerContainer.Layer>
+                <LayerContainer.Layer isFullHeight={false} isFullWidth={false} alignmentVertical={Alignment.Center} alignmentHorizontal={Alignment.Center}>
+                  <Box variant='rounded' shouldClipContent={true} width='130px' height='130px'>
+                    <Image source={imageLink} alternativeText='image' fitType='contain' />
+                  </Box>
+                </LayerContainer.Layer>
+              </LayerContainer>
+            </Box>
+            <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true}>
+              <Button variant='tertiary' text={'change picture'} onClicked={() => setStage(1)} />
+              <Spacing variant={PaddingSize.Wide2} />
+              <Button variant='tertiary' text={'change frame'} />
+            </Stack>
+            <Button variant='secondary' isFullWidth={true} text= {'Set Twitter PFP'} iconGutter={PaddingSize.Wide} iconLeft={<KibaIcon iconId='ion-logo-twitter' />} />
+            <Button variant='secondary' isFullWidth={true} text= {'Set Discord PFP'} iconLeft={<KibaIcon iconId='ion-logo-discord' />} />
+            <Button variant='secondary' isFullWidth={true} text= {'Set ENS PFP'} iconLeft={<KibaIcon iconId='ion-globe' />} />
+            <Button variant='secondary' isFullWidth={true} text= {'Download'} iconLeft={<KibaIcon iconId='ion-download' />} />
+          </Stack>
+        )}
+      </Stack>
+    </ContainingView>
   );
 };
--- app/src/theme.ts
+++ app/src/theme.ts
@@ -21,12 +21,94 @@ export const buildAppTheme = (): ITheme => {
       note: {
         color: '$colors.textClear50',
       },
+      header3: {
+        'font-size': '2.0rem',
+        'font-weight': '500',
+        'margin-bottom': '0.25em',
+        'text-shadow': '0px 0px 5px #F0F0F0',
+      },
     },
     boxes: {
       card: {
         margin: '0',
       },
+      wideBorder: {
+        margin: '0',
+        'box-shadow': '0px 0px 50px 20px rgba(255, 255, 255, 0.35) ',
+      },
     },
+    buttons: {
+      default: {
+        normal: {
+          default: {
+            text: {
+              'font-size': '0.8em',
+            },
+          },
+        },
+      },
+      primary: {
+        normal: {
+          default: {
+            background: {
+              'background-color': 'rgba(255, 255, 255, 0.25)',
+              'border-color': 'rgba(255, 255, 255, 0.3)',
+              'border-width': '1px',
+            },
+            text: {
+              color: '$colors.textOnBrand',
+            },
+          },
+          hover: {
+            background: {
+              'background-color': 'rgba(255, 255, 255, 0.35)',
+            },
+          },
+          press: {
+            background: {
+              'background-color': 'rgba(255, 255, 255, 0.55)',
+            },
+          },
+          focus: {
+            background: {
+              'border-color': 'rgba(255, 255, 255, 0.75)',
+            },
+          },
+        },
+      },
+      secondary: {
+        normal: {
+          default: {
+            background: {
+              'border-color': '#ffffff',
+              'border-width': '1px',
+              'background-color': 'transparent',
+              'box-shadow': '0px 0px 5px 5px rgba(255, 255, 255, 0.35) ',
+            },
+            text: {
+              color: '#FFFFFF',
+              'text-shadow': '0px 0px 5px #FFFFFF ',
+            },
+          },
+          hover: {
+            background: {
+              'background-color': 'rgba(255, 255, 255, 0.35)',
+            },
+          },
+          press: {
+            background: {
+              'background-color': 'rgba(255, 255, 255, 0.55)',
+            },
+          },
+          focus: {
+            background: {
+              'border-color': 'rgba(255, 255, 255, 0.75)',
+            },
+          },
+        },
+      },
+    },
+
     selectableViews: {
       default: {
         normal: {
@@ -45,6 +127,7 @@ export const buildAppTheme = (): ITheme => {
             overlay: baseTheme.linkBases.default.normal.focus.background,
           },
         },
+
         selected: {
           default: {
             overlay: {
