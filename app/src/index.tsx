f60370f21e607fd41606a3694b216c53d71b16fd
Krishan Patel <krishan711@gmail.com>
2022-04-14 18:03:17 +0100

    .

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom';
 
 import { App } from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+if (typeof document !== 'undefined') {
+  const target = document.getElementById('root');
+  const renderMethod = target.hasChildNodes() && window.location.pathname === window.KIBA_STATIC_PATH ? ReactDOM.hydrate : ReactDOM.render;
+  const render = (Component: React.ReactElement): void => {
+    renderMethod(<React.StrictMode><Component /></React.StrictMode>, target);
+  };
+  render(App);
+}
